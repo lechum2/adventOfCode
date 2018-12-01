21b1f6fc003b98a9835205e7efcd723a45811038
Sebastian Lech <lechum2@gmail.com>
2018-12-01 22:18:09 +0100

    Solve the second problem

--- day1/day1.js
+++ day1/day1.js
@@ -2,14 +2,36 @@ var lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('input.txt')
 });
 
-var value = 0;
+var result = 0;
+var inputValues = [];
 
 lineReader.on('line', function (line) {
+    inputValues.push(line);
     if(line[0] == '-') {
-        value -= Number(line.substring(1));
+        result -= Number(line.substring(1));
     } else if (line[0] == '+') {
-        value += Number(line.substring(1));
+        result += Number(line.substring(1));
     }
-    console.log('line was: ', line);
-    console.log('current sum is: ', value);
+});
+
+lineReader.on("close", function () {
+    console.log(inputValues);
+    console.log(result);
+    var values = [];
+    currentValue = 0;
+    var repeat = true;
+    while(repeat) {
+        inputValues.forEach(function (element) {
+            if(element[0] == '-') {
+                currentValue -= Number(element.substring(1));
+            } else if (element[0] == '+') {
+                currentValue += Number(element.substring(1));
+            }
+            if(values.includes(currentValue)) {
+                console.log("first repeated value: ", currentValue);
+                repeat = false;
+            }
+            values.push(currentValue);
+        });
+    }
 });
