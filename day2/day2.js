6e658848c18b5f38ff36bbbd02e8173e34ca2277
Sebastian Lech <lechum2@gmail.com>
2018-12-02 09:43:05 +0100

    Solve second puzzle

--- day2/day2.js
+++ day2/day2.js
@@ -3,8 +3,10 @@ var lineReader = require('readline').createInterface({
 });
 
 let repeatedValuesMap = new Map;
+let boxCodes = [];
 
 lineReader.on('line', function (line) {
+    boxCodes.push(line);
     let repeatedLettersMap = new Map;
     for(let i = 0; i < line.length; i++) {
         let value = 0;
@@ -32,4 +34,20 @@ lineReader.on("close", function () {
         result *= element;
     }
     console.log(result);
+
+    for(let i = 0; i < boxCodes.length; i++) {
+        for(let j = 0; j < boxCodes.length; j++) {
+            if(i == j) continue;
+            let result = '';
+            for(let x = 0; x < boxCodes[j].length; x++) {
+                if(boxCodes[i][x] == boxCodes[j][x]) {
+                    result += boxCodes[i][x];
+                }
+            }
+            if(result.length == (boxCodes[i].length - 1)) {
+                console.log("This is the result: ", result);
+                break;
+            }
+        }
+    }
 });
