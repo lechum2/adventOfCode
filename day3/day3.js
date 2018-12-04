c421c98e0649bfad76a304b37df5ff4b175559ca
Sebastian Lech <lechum2@gmail.com>
2018-12-04 23:23:48 +0100

    Start implementing square object

--- day3/day3.js
+++ day3/day3.js
@@ -1,3 +1,32 @@
 var fs = require('fs');
+
+function Square(inputLine) {
+    let numbers = inputLine.match(/[0-9]+/g);
+    this.index = Number(numbers[0]);
+    this.left = Number(numbers[1]);
+    this.top = Number(numbers[2]);
+    this.right = this.left + Number(numbers[3]);
+    this.bottom = this.top + Number(numbers[4]);
+
+    this.area = function() {
+        return (this.bottom - this.top) * (this.right - this.left);
+    };
+
+    this.getOverlappingSquare = function(otherSquare) {
+        if(this.top > otherSquare.bottom || this.bottom < otherSquare.top)
+            return null;
+        if(this.left > otherSquare.right || this.right < otherSquare.left)
+            return null;
+        
+    }
+};
+
 var inputLines = fs.readFileSync('input.txt').toString().split("\n");
-console.log(inputLines);
+var squares = [];
+for(let inputLine of inputLines) {
+    squares.push(new Square(inputLine));
+}
+console.log(squares[4].area());
+for(let square of squares) {
+
+}
