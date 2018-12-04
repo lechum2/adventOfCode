a634c91e3bfe9669a0de4e63b0eaeb93c91bc79f
Sebastian Lech <lechum2@gmail.com>
2018-12-04 23:34:04 +0100

    change square constructor

--- day3/day3.js
+++ day3/day3.js
@@ -1,12 +1,19 @@
 var fs = require('fs');
 
-function Square(inputLine) {
-    let numbers = inputLine.match(/[0-9]+/g);
-    this.index = Number(numbers[0]);
-    this.left = Number(numbers[1]);
-    this.top = Number(numbers[2]);
-    this.right = this.left + Number(numbers[3]);
-    this.bottom = this.top + Number(numbers[4]);
+function Square(top, left, right, bottom) {
+    this.top = top;
+    this.left = left;
+    this.right = right;
+    this.bottom = bottom;
+
+    this.fromString = function(inputLine) {
+        let numbers = inputLine.match(/[0-9]+/g);
+        this.index = Number(numbers[0]);
+        this.left = Number(numbers[1]);
+        this.top = Number(numbers[2]);
+        this.right = this.left + Number(numbers[3]);
+        this.bottom = this.top + Number(numbers[4]);
+    };
 
     this.area = function() {
         return (this.bottom - this.top) * (this.right - this.left);
@@ -24,9 +31,10 @@ function Square(inputLine) {
 var inputLines = fs.readFileSync('input.txt').toString().split("\n");
 var squares = [];
 for(let inputLine of inputLines) {
-    squares.push(new Square(inputLine));
+    let square = new Square();
+    square.fromString(inputLine);
+    squares.push(square);
 }
-console.log(squares[4].area());
 for(let square of squares) {
 
 }
