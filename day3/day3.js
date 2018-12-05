8fbb0db790979f36a84c71f0bbae846208ec6f56
Sebastian Lech <lechum2@gmail.com>
2018-12-05 13:15:49 +0100

    Solve puzzle 1 by brute force - go pixel by pixel

--- day3/day3.js
+++ day3/day3.js
@@ -11,21 +11,24 @@ function Square(top, left, right, bottom) {
         this.index = Number(numbers[0]);
         this.left = Number(numbers[1]);
         this.top = Number(numbers[2]);
-        this.right = this.left + Number(numbers[3]);
-        this.bottom = this.top + Number(numbers[4]);
+        this.right = this.left + Number(numbers[3] - 1);
+        this.bottom = this.top + Number(numbers[4] - 1);
     };
 
-    this.area = function() {
-        return (this.bottom - this.top) * (this.right - this.left);
+    this.contains = function(x, y) {
+        if(x >= this.left && x <= this.right && y >= this.top && y <= this.bottom) {
+            return true;
+        }
+        return false;
     };
-
-    this.getOverlappingSquare = function(otherSquare) {
+    this.isOverlapping = function(otherSquare) {
         if(this.top > otherSquare.bottom || this.bottom < otherSquare.top)
-            return null;
+            return false;
         if(this.left > otherSquare.right || this.right < otherSquare.left)
-            return null;
-        
-    }
+            return false;
+
+        return true;
+    };
 };
 
 var inputLines = fs.readFileSync('input.txt').toString().split("\n");
@@ -35,6 +38,22 @@ for(let inputLine of inputLines) {
     square.fromString(inputLine);
     squares.push(square);
 }
-for(let square of squares) {
+let maxWidth = Math.max(...squares.map(square => square.right));
+let maxHeight = Math.max(...squares.map(square => square.bottom));
 
+let result = 0;
+for(let x = 0; x < maxWidth + 1; x++) {
+    for(let y = 0; y < maxHeight + 1; y++) {
+        let contains = 0;
+        for(let square of squares) {
+            if(square.contains(x, y)) {
+                contains++;
+            }
+            if(contains >= 2) {
+                result++;
+                break;
+            }
+        }
+    }
 }
+console.log(result);
