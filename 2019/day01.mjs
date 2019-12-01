4ff1d6b5f2cce3eb193c39244f58baef579607f6
Sebastian Lech <lechum2@gmail.com>
2019-12-01 23:41:03 +0100

    Solve problem 2 for day1

--- 2019/day01.mjs
+++ 2019/day01.mjs
@@ -3,6 +3,12 @@ import {getInput} from './common/inputReader.mjs';
 let data = getInput('day01.txt');
 data.unshift(0);
 
-console.log(data.reduce((sum, value) => {
-    return sum + Math.floor(value / 3) - 2;
+console.log(data.reduce((sum, mass) => {
+    let fuel = Math.floor(mass / 3) - 2;
+    let sumaryMas = sum;
+    while(fuel > 0) {
+        sumaryMas += fuel;
+        fuel = Math.floor(fuel / 3) - 2;
+    }
+    return sumaryMas;
 }));
