43b57cfb29ca409e980279291bce1fb8627e8425
Sebastian Lech <lechum2@gmail.com>
2019-12-01 23:43:05 +0100

    Formating fix - for consistency

--- 2019/common/inputReader.mjs
+++ 2019/common/inputReader.mjs
@@ -1,5 +1,5 @@
 import fs from 'fs';
 
 export function getInput(fileName) {
-    return fs.readFileSync(fileName).toString().split("\n");
+    return fs.readFileSync(fileName).toString().split('\n');
 }
