1c0f5a4744785b899fcf02a2b9a364ad50faf392
Sebastian Lech <lechum2@gmail.com>
2019-12-03 11:48:54 +0100

    Beginning of solution for day 3

---
+++ 2019/day03.mjs
@@ -0,0 +1,19 @@
+import {getInput} from './common/inputReader.mjs'
+
+class Grid {
+	constructor(size, centralPortIndex){
+		this.size = size;
+		this.centralPortIndex = centralPortIndex;
+	}
+	const innerGrid = new Array[][];
+	const size = 2000;
+	const centralPort = 1000;
+	
+	function addWire(wire){
+
+	}
+}
+
+var wires = getInput('day03.txt');
+
+
