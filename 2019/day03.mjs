f3c1159da981438a4e84446e5d098687bbf76be6
Sebastian Lech <lechum2@gmail.com>
2019-12-03 12:49:58 +0100

    Start parsing of wire directions

--- 2019/day03.mjs
+++ 2019/day03.mjs
@@ -1,19 +1,25 @@
 import {getInput} from './common/inputReader.mjs'
 
 class Grid {
-	constructor(size, centralPortIndex){
-		this.size = size;
-		this.centralPortIndex = centralPortIndex;
-	}
-	const innerGrid = new Array[][];
-	const size = 2000;
-	const centralPort = 1000;
-	
-	function addWire(wire){
+    constructor(size, centralPortIndex) {
+        this.size = size;
+        this.centralPortIndex = centralPortIndex;
+        this.innerGrid = new Array();
+    }
 
-	}
+    addWire(wire) {
+        let wireArray = wire.toString().split(',');
+        for (const element of wireArray) {
+            const direction = element.charAt(0);
+            let value = element.substr(1);
+            value = parseInt(value);
+            console.log(direction);
+            console.log(value);
+        }
+    }
 }
 
 var wires = getInput('day03.txt');
-
+var grid = new Grid(2000, 1000);
+grid.addWire(wires[0]);
 
