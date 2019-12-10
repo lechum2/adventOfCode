6f5b897a33418c6178757a7f1bcabe667de7d86e
Sebastian Lech <lechum2@gmail.com>
2019-12-10 09:59:19 +0100

    Move intcode computer to common

---
+++ 2019/common/IntcodeComputer.mjs
@@ -0,0 +1,18 @@
+export function compute(data) {
+    let index = 0;
+    while(index < data.length) {
+        let operator = data[index];
+        let arg1 = data[data[index + 1]];
+        let arg2 = data[data[index + 2]];               let result;                                     switch(operator) {                                  case 1:
+                result = arg1 + arg2;
+                break;
+            case 2:
+                result = arg1 * arg2;
+                break;
+            case 99:
+                return;
+            default:                                            console.error('Unknown operator!');
+                return;
+        }                                               data[data[index + 3]] = result;
+        index += 4;                                 }
+}
--- 2019/day02.mjs
+++ 2019/day02.mjs
@@ -1,29 +1,5 @@
 import {getInput} from './common/inputReader.mjs';
-
-function compute(data) {
-    let index = 0;
-    while(index < data.length) {
-        let operator = data[index];
-        let arg1 = data[data[index + 1]];
-        let arg2 = data[data[index + 2]];
-        let result;
-        switch(operator) {
-            case 1:
-                result = arg1 + arg2;
-                break;
-            case 2:
-                result = arg1 * arg2;
-                break;
-            case 99:
-                return;
-            default:
-                console.error('Unknown operator!');
-                return;
-        }
-        data[data[index + 3]] = result;
-        index += 4;
-    }
-}
+import {compute} from './common/IntcodeComputer.mjs';
 
 function findInputsForValue(data, value) {
     for(let i=0; i<100; i++) {
@@ -46,6 +22,6 @@ data[2] = 2;
 compute(data);
 console.log(data[0]);
 
-var nounAndVerb = findInputsForValue(input, 19690720);
-console.log(nounAndVerb);
+//var nounAndVerb = findInputsForValue(input, 19690720);
+//console.log(nounAndVerb);
 
