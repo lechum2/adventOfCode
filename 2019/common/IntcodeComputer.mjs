d7de70ddde1428928196fe83ac04d0cd21a75ed2
Sebastian Lech <lechum2@gmail.com>
2019-12-11 10:36:48 +0100

    Format with prettier

--- 2019/common/IntcodeComputer.mjs
+++ 2019/common/IntcodeComputer.mjs
@@ -6,10 +6,10 @@ export class IntcodeComputer {
     compute(input = 0) {
         let output = 0;
         let index = 0;
-        while (index < this.data.length) {
-            let operator = this.data[index];
-            let arg1 = this.data[this.data[index + 1]];
-            let arg2 = this.data[this.data[index + 2]];
+        while (index < data.length) {
+            let operator = data[index];
+            let arg1 = data[data[index + 1]];
+            let arg2 = data[data[index + 2]];
             let result;
             switch (operator) {
                 case 1:
@@ -29,7 +29,7 @@ export class IntcodeComputer {
                     console.error("Unknown operator!");
                     return;
             }
-            this.data[this.data[index + 3]] = result;
+            data[data[index + 3]] = result;
             index += 4;
         }
         return output;
--- 2019/common/inputReader.mjs
+++ 2019/common/inputReader.mjs
@@ -1,5 +1,9 @@
-import fs from 'fs';
+import fs from "fs";
 
-export function getInput(fileName, separator = '\n', type = String) {
-    return fs.readFileSync(fileName).toString().split(separator).map(type);
+export function getInput(fileName, separator = "\n", type = String) {
+    return fs
+        .readFileSync(fileName)
+        .toString()
+        .split(separator)
+        .map(type);
 }
