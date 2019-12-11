e2a6479f32ff5192e6f97cc7e18fd0661ae4b308
Sebastian Lech <lechum2@gmail.com>
2019-12-11 09:57:38 +0100

    Fix Intcode computer to work as previously

--- 2019/common/IntcodeComputer.mjs
+++ 2019/common/IntcodeComputer.mjs
@@ -3,36 +3,37 @@ export class IntcodeComputer {
         this.data = data;
     }
 
-function compute(input = 0) {
-    let output = 0;
-    let index = 0;
-    while (index < data.length) {
-        let operator = data[index];
-        let arg1 = data[data[index + 1]];
-        let arg2 = data[data[index + 2]];
-        let result;
-        switch (operator) {
-            case 1:
-                result = arg1 + arg2;
-                break;
-            case 2:
-                result = arg1 * arg2;
-                break;
-            case 3:
-                this.put(input, this.get(index +1));
-                break;
-            case 4:
-                output = this.get(index + 1);
-            case 99:
-                return;
-            default: console.error('Unknown operator!');
-                return;
+    compute(input = 0) {
+        let output = 0;
+        let index = 0;
+        while (index < this.data.length) {
+            let operator = this.data[index];
+            let arg1 = this.data[this.data[index + 1]];
+            let arg2 = this.data[this.data[index + 2]];
+            let result;
+            switch (operator) {
+                case 1:
+                    result = arg1 + arg2;
+                    break;
+                case 2:
+                    result = arg1 * arg2;
+                    break;
+                case 3:
+                    this.put(input, this.get(index +1));
+                    break;
+                case 4:
+                    output = this.get(index + 1);
+                case 99:
+                    return;
+                default: console.error('Unknown operator!');
+                    return;
+            }
+            this.data[this.data[index + 3]] = result;
+            index += 4;
         }
-        data[data[index + 3]] = result;
-        index += 4;
+        return output;
     }
-    return output;
-}
+
     put(value, index) {
         this.data[index] = value;
     }
--- 2019/day02.mjs
+++ 2019/day02.mjs
@@ -1,5 +1,5 @@
 import {getInput} from './common/inputReader.mjs';
-import {compute} from './common/IntcodeComputer.mjs';
+import {IntcodeComputer} from './common/IntcodeComputer.mjs';
 
 function findInputsForValue(data, value) {
     for(let i=0; i<100; i++) {
@@ -7,7 +7,8 @@ function findInputsForValue(data, value) {
             let iterationData = [...data];
             iterationData[1] = i;
             iterationData[2] = j;
-            compute(iterationData);
+            let intcode = new IntcodeComputer(iterationData);
+            intcode.compute(iterationData);
             if(iterationData[0] === value) {
                 return 100 * i + j;
             }
@@ -19,9 +20,10 @@ let input = getInput('day02.txt', ',', Number);
 let data = [...input];
 data[1] = 12;
 data[2] = 2;
-compute(data);
+let intcode = new IntcodeComputer(data);
+intcode.compute(data);
 console.log(data[0]);
 
-//var nounAndVerb = findInputsForValue(input, 19690720);
-//console.log(nounAndVerb);
+var nounAndVerb = findInputsForValue(input, 19690720);
+console.log(nounAndVerb);
 
