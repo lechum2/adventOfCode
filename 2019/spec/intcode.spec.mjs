354e9b9f824966bb7d48d469655e5bba469f4625
Sebastian Lech <lechum2@gmail.com>
2019-12-12 08:49:13 +0100

    Try to run jasmine with modules

--- 2019/spec/intcode.spec.mjs
+++ 2019/spec/intcode.spec.mjs
@@ -1,7 +1,9 @@
-import {Intcode} from "../common/IntcodeComputer.mjs"
+import { Intcode } from "./../common/IntcodeComputer.mjs";
 
 describe("Intcode Computer Unit Test", () => {
     it("should return input as output", () => {
-        
+        let intcode = new Intcode([3, 0, 4, 0, 99]);
+        let result = intcode.compute(313);
+        expect(result).toBe(313);
     });
 });
