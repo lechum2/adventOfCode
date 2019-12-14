62a7dfdd4249b7599f3827064fabb9c0a045a84a
Sebastian Lech <lechum2@gmail.com>
2019-12-14 22:06:05 +0100

    Attempt to use jest with babel for tests

---
+++ 2019/babel.config.js
@@ -0,0 +1,21 @@
+// babel.config.js
+module.exports = {
+    presets: [
+        [
+            "@babel/preset-env",
+            {
+                targets: {
+                    node: "current"
+                }
+            }
+        ]
+    ],
+    plugins: [
+        [
+            "module-extension",
+            {
+                mjs: "js"
+            }
+        ]
+    ]
+};
---
+++ 2019/common/IntcodeComputer.test.js
@@ -0,0 +1,7 @@
+import { Intcode } from "./IntcodeComputer.mjs";
+
+test("should return input as output", () => {
+    let intcode = new Intcode([3, 0, 4, 0, 99]);
+    let result = intcode.compute(313);
+    expect(result).toBe(313);
+});
