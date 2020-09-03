c86af017813e65d5979d508501cc9cdef72a8daa
Sebastian Lech <lechum2@gmail.com>
2020-09-03 12:48:50 +0200

    Counting visible asteroids for coordinets - not working properly

--- 2019/day10.js
+++ 2019/day10.js
@@ -4,12 +4,11 @@ const asteroids = getInput("day10test1.txt", "\n", String);
 const height = asteroids.length;
 const width = asteroids[0].length;
 
-
 function isAsteroid(x, y) {
     if (x < 0 || x >= width || y < 0 || y >= height) {
         return false;
     }
-    return asteroids[y][y] === "#" ? true : false;
+    return asteroids[y][x] === "#" ? true : false;
 }
 
 function countVisibleFor(x, y) {
@@ -54,13 +53,28 @@ function countVisibleLeftFor(x, y) {
         let b = y + d;
         let c = 0;
         let a = x + c;
-        for(c = 0; c >= -radius; c--) {
+        for (c = 0; c >= -radius; c--) {
             a = x + c;
             if (isAsteroid(a, b)) {
                 angles.add(c / d);
             }
         }
+        c = -radius;
+        for (d = -radius + 1; d <= radius; d++) {
+            b = y + d;
+            if (isAsteroid(a, b)) {
+                d === 0 ? angles.add(-1) : angles.add(c / d);
+            }
+        }
+        d = radius;
+        for (c = -radius + 1; c < 0; c++) {
+            a = x + c;
+            if (isAsteroid(a, b)) {
+                angles.add(c / d);
+            }
+        }
+    }
+    return angles.size;
 }
 
-
-console.log(countVisibleRightFor(1, 0));
+console.log(countVisibleFor(4, 4));
