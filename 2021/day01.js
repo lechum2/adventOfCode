4888b8b03236e1867b867d030f8e8aa226788e51
Sebastian Lech <lechum2@gmail.com>
2021-12-02 23:26:56 +0100

    Solve for day 1 part 2

--- 2021/day01.js
+++ 2021/day01.js
@@ -3,11 +3,19 @@ import { getInput } from "../2019/common/inputReader.js";
 let data = getInput("day01.txt", "\n", Number);
 
 let count = 0;
+let sumCount = 0;
 
-for(let i = 1; i < data.length; i++) {
-    if(data[i - 1] < data[i]) {
+for (let i = 1; i < data.length; i++) {
+    if (data[i - 1] < data[i]) {
         count++;
+    }
+    if (i < 3) {
+        continue;
+    }
+    if (data[i - 3] + data[i - 2] + data[i - 1] < data[i - 2] + data[i - 1] + data[i]) {
+        sumCount++;
     }
 }
 
 console.log(count);
+console.log(sumCount);
