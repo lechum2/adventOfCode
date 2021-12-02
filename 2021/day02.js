fadf1c403ad967bb7d22a51aa61aa3f1e15e7e2a
Sebastian Lech <lechum2@gmail.com>
2021-12-02 23:42:47 +0100

    Solve day 2 part 2

--- 2021/day02.js
+++ 2021/day02.js
@@ -4,18 +4,26 @@ let data = getInput("day02.txt");
 data = data.map((value) => value.split(" "));
 let horizontalPosition = 0;
 let depth = 0;
+let aim = 0;
+let aimDepth = 0;
+
 data.forEach((step) => {
+    const stepValue = Number.parseInt(step[1]);
     switch (step[0]) {
         case "forward":
-            horizontalPosition += Number.parseInt(step[1]);
+            horizontalPosition += stepValue;
+            aimDepth += aim * stepValue;
             break;
         case "up":
-            depth -= Number.parseInt(step[1]);
+            depth -= stepValue;
+            aim -= stepValue;
             break;
         case "down":
-            depth += Number.parseInt(step[1]);
+            depth += stepValue;
+            aim += stepValue;
             break;
     }
 });
 
 console.log(horizontalPosition * depth);
+console.log(horizontalPosition * aimDepth);
