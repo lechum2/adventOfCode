20c0082722fed930bef8f04a97f2db77f2426096
Sebastian Lech <lechum2@gmail.com>
2022-08-23 11:10:41 +0200

    solve day 7 problem 2

--- 2021/day07.js
+++ 2021/day07.js
@@ -8,7 +8,7 @@ function getMinFuel(input) {
     for(let i = 0; i <= topValue; i++) {
         let currentFuel = 0;
         for(const value of input) {
-            currentFuel += Math.abs(value - i);
+            currentFuel += getIncreasingValue(Math.abs(value - i));
         }
         if (currentFuel < fuel) {
             fuel = currentFuel;
@@ -16,6 +16,17 @@ function getMinFuel(input) {
         console.log(fuel);
     }
 }
+
+function getIncreasingValue(input) {
+    let result = 0;
+    let value = 0;
+    for (let i = 1; i <= input; i++) {
+        value++;
+        result += value;
+    }
+    return result;
+}
+
 
 let data = getInput("day07.txt", ",", Number);
 getMinFuel(data);
