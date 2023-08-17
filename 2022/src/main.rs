d0577c9ec24b85199060592692169b771c0cceb9
Sebastian Lech <lechum2@gmail.com>
2023-08-17 23:45:21 +0200

    solve day 1 part 1

--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -1,7 +1,22 @@
 mod http_input_reader;
 
 fn main() {
-    println!("Hello, world!");
     let input_vector = http_input_reader::get_input(2022, 1, "\n");
     println!("{:?}", input_vector);
+    let mut max_total_calories = 0;
+    let mut elf_total_calories = 0;
+    for line in input_vector {
+        if line == "" {
+            if elf_total_calories > max_total_calories {
+                max_total_calories = elf_total_calories;
+            }
+            elf_total_calories = 0;
+        } else {
+            elf_total_calories += line.parse::<i32>().unwrap();
+        }
+    }
+    if elf_total_calories > max_total_calories {
+        max_total_calories = elf_total_calories;
+    }
+    println!("{:?}", max_total_calories);
 }
