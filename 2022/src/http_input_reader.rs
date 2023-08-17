e79cb1a569179781776e9977e47b975f2141beda
Sebastian Lech <lechum2@gmail.com>
2023-08-17 11:17:05 +0200

    Play with rust - input reader is still not working

--- 2022/src/http_input_reader.rs
+++ 2022/src/http_input_reader.rs
@@ -1,17 +1,23 @@
-    use std::fs;
-    use reqwest::header;
+use reqwest::header;
+use std::fs;
 
-    pub fn get_input(year: i32, day: i32, separator: String) {
-        let token = fs::read_to_string("token").expect("Could not read token file");
-        let mut header_value = header::HeaderValue::from_str(format!("session={token}").trim()).unwrap();
-        header_value.set_sensitive(true);
-        let mut headers = header::HeaderMap::new();
-        headers.insert("Cookie", header_value);
-        let client = reqwest::blocking::Client::builder()
-            .default_headers(headers)
-            .build()
-            .unwrap();
-        let response = client.get(format!("https://adventofcode.com/{year}/day/{day}/input")).send().expect("Input request failed").text().unwrap();
-        let inputVector = response.split('\n').collect();
-        println!({:?}, inputVector);
-    }
+pub fn get_input(year: i32, day: i32, separator: &str) -> Vec<&str> {
+    let token = fs::read_to_string("token").expect("Could not read token file");
+    let mut header_value =
+        header::HeaderValue::from_str(format!("session={token}").trim()).unwrap();
+    header_value.set_sensitive(true);
+    let mut headers = header::HeaderMap::new();
+    headers.insert("Cookie", header_value);
+    let client = reqwest::blocking::Client::builder()
+        .default_headers(headers)
+        .build()
+        .unwrap();
+    let response = client
+        .get(format!("https://adventofcode.com/{year}/day/{day}/input"))
+        .send()
+        .expect("Input request failed")
+        .text()
+        .unwrap();
+    let input_vector: Vec<&str> = response.split(separator).collect().clone();
+    input_vector
+}
--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -2,5 +2,7 @@ mod http_input_reader;
 
 fn main() {
     println!("Hello, world!");
-    http_input_reader::get_input(2022, 1, "\n".to_string());
+    http_input_reader::get_input(2022, 1, "\n");
+    let input_vector: Vec<&str> = response.split(separator).collect();
+    println!("{:?}", input_vector);
 }
