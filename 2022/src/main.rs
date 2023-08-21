84127653210fd6df1f11ed09c1f53e9072f06d79
Sebastian Lech <lechum2@gmail.com>
2023-08-21 19:33:09 +0200

    solve day 1 part 2

--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -2,21 +2,23 @@ mod http_input_reader;
 
 fn main() {
     let input_vector = http_input_reader::get_input(2022, 1, "\n");
-    println!("{:?}", input_vector);
-    let mut max_total_calories = 0;
-    let mut elf_total_calories = 0;
+    let mut elf_total_calories = Vec::new();
+    let mut single_elf_calories = 0;
     for line in input_vector {
         if line == "" {
-            if elf_total_calories > max_total_calories {
-                max_total_calories = elf_total_calories;
-            }
-            elf_total_calories = 0;
+            elf_total_calories.push(single_elf_calories);
+            single_elf_calories = 0;
         } else {
-            elf_total_calories += line.parse::<i32>().unwrap();
+            single_elf_calories += line.parse::<i32>().unwrap();
         }
     }
-    if elf_total_calories > max_total_calories {
-        max_total_calories = elf_total_calories;
-    }
-    println!("{:?}", max_total_calories);
+    elf_total_calories.sort();
+    elf_total_calories.reverse();
+    print!("Elf with max calories has {:?}", elf_total_calories.get(0).unwrap());
+    print!(
+        "Top 3 elfs have {:?} calories",
+        elf_total_calories.get(0).unwrap()
+        + elf_total_calories.get(1).unwrap()
+        + elf_total_calories.get(2).unwrap()
+    );
 }
