23cba0cbfed09416f6d9debdc6eaebe8c2d10e9a
Sebastian Lech <y2dx@nykredit.dk>
2023-10-09 10:53:41 +0200

    Add comparison funciton

--- 2022/src/bin/day2.rs
+++ 2022/src/bin/day2.rs
@@ -3,6 +3,7 @@ enum GameChoise {
     Paper,
     Scissors
 }
+
 impl GameChoise {
     fn points(&self) -> u8 {
         match self {
@@ -11,7 +12,34 @@ impl GameChoise {
             GameChoise::Scissors => 3
         }
     }
+
+    fn scoreWith(&self, other: &GameChoise) -> u8 {
+        match self {
+            GameChoise::Rock => {
+                match other {
+                    GameChoise::Rock => 3,
+                    GameChoise::Paper => 0,
+                    GameChoise::Scissors => 6,
+                }
+            }
+            GameChoise::Paper => {
+                match other {
+                    GameChoise::Rock => 6,
+                    GameChoise::Paper => 3,
+                    GameChoise::Scissors => 0,
+                }
+            }
+            GameChoise::Scissors => {
+                match other {
+                    GameChoise::Rock => 0,
+                    GameChoise::Paper => 6,
+                    GameChoise::Scissors => 3,
+                }
+            }
+        }
+    }
 }
+
 fn main() {
     let input_vector = input_reader::get_input(2022, 2, "\n");
     let mut score = 0;
