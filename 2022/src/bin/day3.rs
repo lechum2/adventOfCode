8d4b3f28ba0b5b4e0ae78b56ccd6d12be5157338
Sebastian Lech <lechum2@gmail.com>
2023-10-13 22:32:40 +0200

    Solve day 3 part 1

--- 2022/src/bin/day3.rs
+++ 2022/src/bin/day3.rs
@@ -1,28 +1,82 @@
+use std::collections::HashSet;
+
 #[cfg(test)]
 mod day3_test {
+    use std::collections::HashSet;
+
+    #[test]
+    fn when_given_string_should_find_all_duplicated_chars() {
+        let input = "vlJrwpWtwJgWrhcsFMMfFFhFpl";
+        let result = crate::get_duplicated_chars(&input);
+        let mut expected = HashSet::new();
+        expected.insert('l');
+        expected.insert('p');
+        assert_eq!(result, expected);
+    }
+
     #[test]
     fn when_given_string_should_find_duplicated_char() {
-        let input = "vJrwpWtwJgWrhcsFMMfFFhFp";
-        let result = crate::get_duplicated_char(&input);
-        assert_eq!(result, 'p');
+        let input = "cvvcll";
+        let result = crate::get_duplicated_chars(&input);
+        let mut expected = HashSet::new();
+        expected.insert('c');
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn when_given_duplicates_at_corners_should_find_duplicated_char() {
+        let input = "cvxxzc";
+        let result = crate::get_duplicated_chars(&input);
+        let mut expected = HashSet::new();
+        expected.insert('c');
+        expected.insert('x');
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn when_given_char_should_return_proper_value() {
+        let input = 'Z';
+        let result = crate::get_char_value(input);
+        assert_eq!(result, 52);
     }
 }
 
-fn get_duplicated_char(line: &str) -> char {
+fn get_duplicated_chars(line: &str) -> HashSet<char> {
     let chars: Vec<char> = line.chars().collect();
     let half_len = chars.len() / 2;
-    for i in 0..half_len - 1 {
-        for j in 1..half_len {
-            let left_char = *chars.get(i).unwrap();
+    let mut result = HashSet::new();
+    for i in 0..half_len {
+        let left_char = *chars.get(i).unwrap();
+        for j in 0..half_len {
             let right_char = *chars.get(half_len + j).unwrap();
-            println!("left is {left_char} and right is {right_char}");
             if left_char == right_char {
-                return *chars.get(i).unwrap();
+                result.insert(left_char);
             }
         }
     }
-    panic!("No duplicate char found!");
+    return result;
+}
+
+fn get_char_value(letter: char) -> u32 {
+    if letter >= 'a' && letter <= 'z' {
+        return letter as u32 - 'a' as u32 + 1;
+    }
+    else if letter >= 'A' && letter <= 'Z' {
+        return letter as u32 - 'A' as u32 + 27;
+    }
+    panic!("Not supported character: {letter}");
 }
 
 fn main() {
+    let input_vector = input_reader::get_input(2022, 3, "\n");
+    let mut result: u32 = 0;
+    for line in input_vector {
+        if line.is_empty() {
+            continue;
+        }
+        for duplicate in get_duplicated_chars(&line) {
+            result += get_char_value(duplicate);
+        }
+    }
+    println!("Result is: {result}");
 }
