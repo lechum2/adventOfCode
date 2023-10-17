53f0cb20e4d688609b4b8a4eb9e56e8345098acd
Sebastian Lech <lechum2@gmail.com>
2023-10-17 15:41:11 +0200

    Beginning of solution for day 5

---
+++ 2022/src/bin/day5.rs
@@ -0,0 +1,15 @@
+use std::collections::BinaryHeap;
+
+struct Cargo {
+    stacks: Vec<BinaryHeap<char>>
+}
+
+impl Cargo {
+    fn from(input: Vec<&str>) -> Self {
+
+    }
+}
+
+fn main() {
+    let input_vector = input_reader::get_input(2022, 5, "\n");
+}
