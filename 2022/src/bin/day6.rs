e11416d62ddccba5b3866d17bfdc8058850c5abe
Sebastian Lech <lechum2@gmail.com>
2023-10-20 13:13:11 +0200

    Different method of checking uniqness

--- 2022/src/bin/day6.rs
+++ 2022/src/bin/day6.rs
@@ -1,26 +1,18 @@
-fn get_packet_marker_start(signal: &str) -> usize {
+use std::collections::HashSet;
+
+fn get_packet_marker_start(signal: &str, unique_num: usize) -> usize {
     let chars: Vec<char> = signal.chars().collect();
-    for i in 3..chars.len() {
-        if chars.get(i).unwrap() != chars.get(i - 1).unwrap()
-            && chars.get(i).unwrap() != chars.get(i - 2).unwrap()
-            && chars.get(i).unwrap() != chars.get(i - 3).unwrap()
-            && chars.get(i - 1).unwrap() != chars.get(i - 2).unwrap()
-            && chars.get(i - 1).unwrap() != chars.get(i - 3).unwrap()
-            && chars.get(i - 2).unwrap() != chars.get(i - 3).unwrap()
-        {
+    for i in (unique_num - 1)..chars.len() {
+        let mut uniq = HashSet::new();
+        let mut all_uniq = true;
+        for j in 0..unique_num {
+            all_uniq &= uniq.insert(chars.get(i - j).unwrap().clone());
+        }
+        if all_uniq {
             return i + 1;
         }
     }
     return chars.len();
-}
-
-fn has_unique_elements<T>(iter: T) -> bool
-where
-    T: IntoIterator,
-    T::Item: Eq + Hash,
-{
-    let mut uniq = HashSet::new();
-    iter.into_iter().all(move |x| uniq.insert(x))
 }
 
 fn main() {
@@ -29,7 +21,7 @@ fn main() {
         if line.is_empty() {
             continue;
         }
-        let result = get_packet_marker_start(line.as_str());
+        let result = get_packet_marker_start(line.as_str(), 4);
         println!("Marker found at: {}", result);
     }
 }
@@ -38,13 +30,13 @@ fn main() {
 mod day6_test {
     #[test]
     fn should_find_marker_at_5() {
-        let result = crate::get_packet_marker_start("bvwbjplbgvbhsrlpgdmjqwftvncz");
+        let result = crate::get_packet_marker_start("bvwbjplbgvbhsrlpgdmjqwftvncz", 4);
         assert_eq!(5, result);
     }
 
     #[test]
     fn should_find_marker_at_11() {
-        let result = crate::get_packet_marker_start("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw");
+        let result = crate::get_packet_marker_start("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4);
         assert_eq!(11, result);
     }
 }
