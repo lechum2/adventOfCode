48d9f50ed630715b7bcaf77c07ec691727bcb26d
Sebastian Lech <lechum2@gmail.com>
2023-10-24 12:34:49 +0200

    Add lifetime and size implementation

--- 2022/src/bin/day7.rs
+++ 2022/src/bin/day7.rs
@@ -1,21 +1,22 @@
-struct Directory {
-    parent: &Directory;
-    sub_directories: Vec<&Directory>;
-    files: Vec<&File>;
-    name: &str;
+struct Directory<'a> {
+    parent: &'a Directory<'a>,
+    sub_directories: Vec<&'a Directory<'a>>,
+    files: Vec<&'a File<'a>>,
+    name: &'a str,
 }
 
-struct File {
-    parent: &Directory;
-    name: &str;
-    size: u32;
+struct File<'a> {
+    parent: &'a Directory<'a>,
+    name: &'a str,
+    size: u32,
 }
 
-impl Directory {
-    fn size(&self) {
-        let size: u32 = 0;
-        size += self.sub_directories.reduce();
-        size += self.files.reduce();
+impl Directory<'_> {
+    fn size(&self) -> u32 {
+        let mut size: u32 = 0;
+        size += self.sub_directories.iter().fold(0, |sum, dir| sum + dir.size());
+        size += self.files.iter().fold(0, |sum, file| sum + file.size);
+        return size;
     }
 }
 
